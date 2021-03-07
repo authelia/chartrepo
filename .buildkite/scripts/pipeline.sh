17bc7c107348c9e356da69db736dab363a56ed54
James Elliott <james-d-elliott@users.noreply.github.com>
2021-03-08 08:03:52 +1100

    fix(authelia): only enable tls secret name when certresolver not configured (#6)

--- .buildkite/scripts/pipeline.sh
+++ .buildkite/scripts/pipeline.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ $BUILDKITE_BRANCH == "master" ]]; then
-  export CR_BYPASS=$(git diff --name-only HEAD~1 | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md)/!{q1}' && echo false || echo true)
+  export CR_BYPASS=$(git diff --name-only HEAD~1 | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md)/{q1}' && echo true || echo false)
 else
-  export CR_BYPASS=$(git diff --name-only `git merge-base --fork-point origin/master` | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md)/!{q1}' && echo false || echo true)
+  export CR_BYPASS=true
 fi
 
 envsubst < .buildkite/pipeline.yaml
