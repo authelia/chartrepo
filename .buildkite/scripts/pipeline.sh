110674fee31fbb0756335c6ad7771cd0dd05be77
James Elliott <james-d-elliott@users.noreply.github.com>
2021-03-30 13:25:04 +1100

    ci: refactor pipeline
    Adds a specific step for testing the version of the chart, allows for some concurrency for building the charts as well as ensures the order is executed correctly, forces deletion of the gh-pages branch locally to hopefully aleviate an out-of-sync git error, and adds a CT_BYPASS env var for future use and testing.

--- .buildkite/scripts/pipeline.sh
+++ .buildkite/scripts/pipeline.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
-if [[ $BUILDKITE_BRANCH == "master" ]]; then
-  export CR_BYPASS=$(git diff --name-only HEAD~1 | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md)/{q1}' && echo true || echo false)
+if [[ "${BUILDKITE_BRANCH}" == "master" ]]; then
+  CHART_CHANGES=$(git diff --name-only HEAD~1 | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md)/{q1}' && echo false || echo true)
+  if [[ "${CHART_CHANGES}" == "true" ]]; then
+    export CR_BYPASS=false
+  else
+    export CR_BYPASS=true
+  fi
 else
+  CHART_CHANGES=$(git diff --name-only `git merge-base --fork-point origin/master` | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md)/{q1}' && echo false || echo true)
   export CR_BYPASS=true
+fi
+
+if [[ "${CHART_CHANGES}" == "true" ]]; then
+  export CT_BYPASS=false
+else
+  export CT_BYPASS=true
 fi
 
 envsubst < .buildkite/pipeline.yaml
