69be206ae8fa772d8bd5b35b4e8f94ebf36f8479
James Elliott <james-d-elliott@users.noreply.github.com>
2021-03-30 13:31:45 +1100

    fix: simplify pipeline env substitution

--- .buildkite/scripts/pipeline.sh
+++ .buildkite/scripts/pipeline.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ "${BUILDKITE_BRANCH}" == "master" ]]; then
-  CHART_CHANGES=$(git diff --name-only HEAD~1 | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md)/{q1}' && echo false || echo true)
-  if [[ "${CHART_CHANGES}" == "true" ]]; then
-    export CR_BYPASS=false
-  else
-    export CR_BYPASS=true
-  fi
+  export CHART_CHANGES=$(git diff --name-only HEAD~1 | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md)/{q1}' && echo false || echo true)
 else
-  CHART_CHANGES=$(git diff --name-only `git merge-base --fork-point origin/master` | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md)/{q1}' && echo false || echo true)
-  export CR_BYPASS=true
-fi
-
-if [[ "${CHART_CHANGES}" == "true" ]]; then
-  export CT_BYPASS=false
-else
-  export CT_BYPASS=true
+  export CHART_CHANGES=$(git diff --name-only `git merge-base --fork-point origin/master` | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md)/{q1}' && echo false || echo true)
 fi
 
 envsubst < .buildkite/pipeline.yaml
