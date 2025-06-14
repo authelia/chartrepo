123f9b77d8747036c9228ee0a5b42c9915adccee
James Elliott <james-d-elliott@users.noreply.github.com>
2025-06-15 06:54:44 +1000

    ci: use makefile (#312)

--- .buildkite/scripts/pipeline.sh
+++ .buildkite/scripts/pipeline.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ "${BUILDKITE_BRANCH}" == "master" ]]; then
-  export CHART_CHANGES=$(git diff --name-only HEAD~1 | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md)/{q1}' && echo false || echo true)
+  export CHART_CHANGES=$(git diff --name-only HEAD~1 | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md|README.md.gotmpl)/{q1}' && echo false || echo true)
 else
-  export CHART_CHANGES=$(git diff --name-only `git merge-base --fork-point origin/master` | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md)/{q1}' && echo false || echo true)
+  export CHART_CHANGES=$(git diff --name-only `git merge-base --fork-point origin/master` | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md|README.md.gotmpl)/{q1}' && echo false || echo true)
 fi
 
 envsubst < .buildkite/pipeline.yaml
