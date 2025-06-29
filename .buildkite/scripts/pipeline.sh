76a4a2b6c71f0f8ca8f20a2c9d5d15b28fb2d008
James Elliott <james-d-elliott@users.noreply.github.com>
2025-06-29 16:34:16 +1000

    build: update helmignore (#336)

--- .buildkite/scripts/pipeline.sh
+++ .buildkite/scripts/pipeline.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ "${BUILDKITE_BRANCH}" == "master" ]]; then
-  export CHART_CHANGES=$(git diff --name-only HEAD~1 | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md|README.md.gotmpl)/{q1}' && echo false || echo true)
+  exoirt CHART_CHANGES=$(git diff --name-only HEAD~1 -- "charts/*/*.md" "charts/*/templates/*" "charts/*/files/*" "charts/*/Chart.yaml" "charts/*/values.yaml" "charts/*/values.*.yaml" "charts/*/values.schema.json" "charts/*/LICENSE" "charts/*/crds/*" "charts/*/Chart.lock" | sed -rn '{q1}' && echo false || echo true)
 else
-  export CHART_CHANGES=$(git diff --name-only `git merge-base --fork-point origin/master` | sed -rn '/^charts\/[a-zA-Z0-9-]+\/(templates\/.*|crds\/.*|Chart.yaml|values.yaml|values.schema.json|LICENSE|README.md|README.md.gotmpl)/{q1}' && echo false || echo true)
+  exoirt CHART_CHANGES=$(git diff --name-only `git merge-base --fork-point origin/master` -- "charts/*/*.md" "charts/*/templates/*" "charts/*/files/*" "charts/*/Chart.yaml" "charts/*/values.yaml" "charts/*/values.*.yaml" "charts/*/values.schema.json" "charts/*/LICENSE" "charts/*/crds/*" "charts/*/Chart.lock" | sed -rn '{q1}' && echo false || echo true)
 fi
 
 envsubst < .buildkite/pipeline.yaml
