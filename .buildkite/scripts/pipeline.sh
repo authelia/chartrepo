e20e1d4cb3fccc9379c3265fc768845ffd5c812d
Brynn Crowley <littlehill723@gmail.com>
2025-06-29 21:26:41 -0700

    fix: typo (#338)
    * fix: typo
    
    Signed-off-by: Brynn Crowley <littlehill723@gmail.com>
    
    * Update .buildkite/scripts/pipeline.sh
    
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Brynn Crowley <littlehill723@gmail.com>
    Signed-off-by: James Elliott <james-d-elliott@users.noreply.github.com>
    Co-authored-by: James Elliott <james-d-elliott@users.noreply.github.com>

--- .buildkite/scripts/pipeline.sh
+++ .buildkite/scripts/pipeline.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 if [[ "${BUILDKITE_BRANCH}" == "master" ]]; then
-  exoirt CHART_CHANGES=$(git diff --name-only HEAD~1 -- "charts/*/*.md" "charts/*/templates/*" "charts/*/files/*" "charts/*/Chart.yaml" "charts/*/values.yaml" "charts/*/values.*.yaml" "charts/*/values.schema.json" "charts/*/LICENSE" "charts/*/crds/*" "charts/*/Chart.lock" | sed -rn '{q1}' && echo false || echo true)
+  export CHART_CHANGES=$(git diff --name-only HEAD~1 -- "charts/*/*.md" "charts/*/templates/*" "charts/*/files/*" "charts/*/Chart.yaml" "charts/*/values.yaml" "charts/*/values.*.yaml" "charts/*/values.schema.json" "charts/*/LICENSE" "charts/*/crds/*" "charts/*/Chart.lock" | sed -rn '{q1}' && echo false || echo true)
 else
-  exoirt CHART_CHANGES=$(git diff --name-only `git merge-base --fork-point origin/master` -- "charts/*/*.md" "charts/*/templates/*" "charts/*/files/*" "charts/*/Chart.yaml" "charts/*/values.yaml" "charts/*/values.*.yaml" "charts/*/values.schema.json" "charts/*/LICENSE" "charts/*/crds/*" "charts/*/Chart.lock" | sed -rn '{q1}' && echo false || echo true)
+  export CHART_CHANGES=$(git diff --name-only `git merge-base --fork-point origin/master` -- "charts/*/*.md" "charts/*/templates/*" "charts/*/files/*" "charts/*/Chart.yaml" "charts/*/values.yaml" "charts/*/values.*.yaml" "charts/*/values.schema.json" "charts/*/LICENSE" "charts/*/crds/*" "charts/*/Chart.lock" | sed -rn '{q1}' && echo false || echo true)
 fi
 
 envsubst < .buildkite/pipeline.yaml
+
